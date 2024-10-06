de54df6d4770945839dc2318e4b067ab24a4c98c
inhart <3300267+inhart@users.noreply.github.com>
2024-10-06 11:10:32 +0200

    last

--- IDD-mine.py
+++ IDD-mine.py
@@ -1,17 +1,43 @@
+
 from pymongo import MongoClient
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
 
+
+
+def log(message):
+	with open('log.txt','a') as f:
+		try:
+			f.write(f'{datetime.now()} '+ f' {message}\n')
+		except:
+			print('error al guardar el log')
+
+def save_to_csv(pelicula):
+
+	with open('peliculas.csv', 'a', encoding='utf-8') as f:
+		for dato in pelicula:
+			try:
+				if pelicula.index(dato) == (len(pelicula)-1):
+					f.write(dato.encode('ascii') + '\n')
+				else:
+					f.write(f'{dato},')
+			except:
+				print('error en el csv\n')
+				print(dato)
 
 def peticion(url):
 	response = requests.get(url)
 	if response.status_code == 200:
 		return response
+	else:
+		log('Request error: ' + str(response.status_code))
+		return None
 
 
 def pelis_ingesta():
-	dic = {}
 
+	pelicula = []
 	url = "https://www.blogdepelis.top/"
 	response = peticion(url)
 	soup = BeautifulSoup(response.content, "html.parser")
@@ -37,12 +63,23 @@ def pelis_ingesta():
 					link4 = a.find('a')['href']
 					peli = peticion(link4)
 					soup4 = BeautifulSoup(peli.content,'html.parser')
-					scr = soup4.find_all('meta')
-					for sc in scr:
+					scr = soup4.find_all('div','separator')
+					if len(scr) == 0:
+						scr = soup4.find_all('p')[1]
+						comentario = scr.get_text()
+					else:
+						comentario = scr[-1].get_text().split('\n')[0]
+					titulo = nombre[:-6]
+					c = cate.get_text()
+					ano = nombre[-5:-1]
 
-						if "description" in sc:
-							print(sc)
-					print(scr)
+					#pelicula.append([titulo, ano, c, comentario])
+					#print(pelicula[len(pelicula)-1])
+					save_to_csv([titulo, ano, c, comentario])
+	return pelicula
+
+
+
 
 
 def api_ingesta():
@@ -55,8 +92,4 @@ mg = MongoClient("localhost", 27017)
 apidb = mg.IDD['api']
 pelidb = mg.IDD['peliculas']
 
-
 peliculas = pelis_ingesta()
-
-
-
