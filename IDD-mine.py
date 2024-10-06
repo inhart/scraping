d90703a6d85398c7fb29b9069060e378a9dd1abc
inhart <3300267+inhart@users.noreply.github.com>
2024-10-06 15:33:03 +0200

    mngoid

--- IDD-mine.py
+++ IDD-mine.py
@@ -3,19 +3,95 @@ from pymongo import MongoClient
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
-import pandas as pd
 
+def peticion(url):
+	response = requests.get(url)
+	if response.status_code == 200:
+		return response
+	else:
+		log('Request error: ' + str(response.status_code))
 
 def log(message):
 	with open('log.txt','a') as f:
 		try:
 			f.write(f'{datetime.now()} '+ f' {message}\n')
-		except:
-			print('error al guardar el log')
+		except Exception() as e:
+			print(f'error al guardar el log {e}')
 
 def amongo(db, film):
 	post = db.insert_one(film)
-	print(post)
+	return post
+
+def incr():
+	global j
+	j+=1
+
+
+def filmscrapy(enlace, name, cat, db, feat):
+	peli = peticion(enlace)
+	soup4 = BeautifulSoup(peli.content, feat)
+	scr = soup4.find_all('div', 'separator')
+	react = soup4.find_all('span', "count-num")
+	emo = []
+	for num in react:
+		num = num.get_text()
+		emo.append(num)
+	if len(scr) == 0:
+		scr = soup4.find_all('p')[1]
+		comentario = scr.get_text()
+	else:
+		comentario = scr[-1].get_text().split('\n')[0]
+
+	film = {
+		'_id': j,
+		'titulo': name[:-7],
+		'year': name[-5:-1],
+		'categoria': cat,
+		'like': emo[0],
+		'dislike': emo[1],
+		'love': emo[2],
+		'shit': emo[3],
+		'link': enlace,
+		'comentario': comentario
+	}
+	amongo(db, film)
+	incr()
+	print(film['_id'])
+
+
+def correpaginas(db, enlace, cate, feat='html.parser'):
+
+	response3 = peticion(enlace)
+	soup3 = BeautifulSoup(response3.content, feat)
+	ases = soup3.find_all('div', 'latestPost-inner')
+	for a in ases:
+		# Para cada pelicula de la pagina
+		nombre = a.find('a')['title']
+		link = a.find('a')['href']
+		filmscrapy(link, nombre, cate, db, feat)
+
+
+
+
+def pelis_ingesta(db, feat):
+
+	url = "https://www.blogdepelis.top/"
+	response = peticion(url)
+	soup = BeautifulSoup(response.content, feat)
+	cat = soup.find_all('a')
+	for cate in cat:
+		if "category" in cate['href']:
+			# Este link es una categoria
+			newlink = cate['href']
+			response2 = peticion(newlink)
+			soup2 = BeautifulSoup(response2.content, feat)
+			pagen = soup2.find_all('a', 'page-numbers')
+			pagen = int(pagen[-2].get_text())
+			for i in range(1,pagen):
+				# Para cada pagina en la categoria
+				link = f'{newlink}/page/{i}'
+				catgry = cate.get_text()
+				correpaginas(db, link, catgry, feat)
 
 
 def save_to_csv(pelicula):
@@ -27,91 +103,24 @@ def save_to_csv(pelicula):
 					f.write('"' + f'{dato}' + '"' + '\n')
 				else:
 					f.write(f'{dato},')
-			except:
-				print('error en el csv\n')
-				print(dato)
-
-def peticion(url):
-	response = requests.get(url)
-	if response.status_code == 200:
-		return response
-	else:
-		log('Request error: ' + str(response.status_code))
-		return None
-
-
-def pelis_ingesta(db):
-	j = 0
-	pelicula = []
-	url = "https://www.blogdepelis.top/"
-	response = peticion(url)
-	soup = BeautifulSoup(response.content, "html.parser")
-
-	cat = soup.find_all('a')
-	for cate in cat:
-		if "category" in cate['href']:
-			# This link is a category
-			newlink = cate['href']
-			response2 = peticion(newlink)
-			soup2 = BeautifulSoup(response2.content, 'html.parser')
-			pagen = soup2.find_all('a', 'page-numbers')
-			pagen = int(pagen[-2].get_text())
-			for i in range(1,pagen):
-				# For each page in the category
-				link3 = f'{newlink}/page/{i}'
-				response3 = peticion(link3)
-				soup3 = BeautifulSoup(response3.content,'html.parser')
-				ases = soup3.find_all('div','latestPost-inner')
-				for a in ases:
-					# For each movie in the page
-					nombre = a.find('a')['title']
-					link4 = a.find('a')['href']
-					peli = peticion(link4)
-					soup4 = BeautifulSoup(peli.content,'html.parser')
-					scr = soup4.find_all('div','separator')
-					react = soup4.find_all('span', "count-num")
-					emo = []
-					for num in react:
-						num = num.get_text()
-						emo.append(num)
-					if len(scr) == 0:
-						scr = soup4.find_all('p')[1]
-						comentario = scr.get_text()
-					else:
-						comentario = scr[-1].get_text().split('\n')[0]
-
-
-					film = {
-						'_id': j,
-						'titulo': nombre[:-6],
-						'ano' : nombre[-5:-1],
-						'categoria' : cate.get_text(),
-						'like': emo[0],
-						'dislike': emo[1],
-						'love': emo[2],
-						'shit': emo[3],
-						'comentario' : comentario
-					}
-					#save_to_csv([titulo, ano, c, like, dislike, love, shit, comentario])
-					print(film)
-					amongo(db, film)
-					j+=1
-
-
-	#return pelicula
-
-
-
+			except Exception() as e:
+				print('error en el csv')
+				print(e)
 
 
 def api_ingesta():
 	return " "
 
+def main():
+	# Conecta a MongoDB
+	mg = MongoClient("localhost", 27017)
+	feat = 'html.parser'
+	pelidb = mg.IDD['blog']
 
-# Connect to MongoDB
-mg = MongoClient("localhost", 27017)
+	pelis_ingesta(pelidb, feat)
 
-apidb = mg.IDD['api']
-pelidb = mg.IDD['blog']
+if __name__ == '__main__':
 
-pelis_ingesta(pelidb)
+	j=0
+
+	main()
